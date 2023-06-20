fce4f8084e0a8fe59477c661348a8458528efabf
Daniel Sousa <daniel.s@crowdbotics.com>
2023-06-20 23:50:21 +0100

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,5 +1,3 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "tst-daniel-v2-dev-91169",
   url: "https://tst-daniel-v2-dev-91169.botics.co",
@@ -7,9 +5,9 @@ export const globalOptions = {
   screenOptions: {
     headerShown: false
   }
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
